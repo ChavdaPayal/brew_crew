278613375d3ddd8bbfa2bb606553aefab1e1ecec
ChavdaPayal <75468124+ChavdaPayal@users.noreply.github.com>
2020-12-04 12:14:06 +0530

    fgh

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+  // This widget is the root of yghdfhgffgfuour application.
   @override
   Widget build(BuildContext context) {
     return StreamProvider<User>.value(
